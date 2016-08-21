e1ea9ccb98678d017b4bf67cc84242b2cf89efe3
jalen002 <jhalvey08@gmail.com>
2016-08-21 16:46:20 -0700

    added roster json object for rosters

--- controllers/rosterController.js
+++ controllers/rosterController.js
@@ -1,3 +1,11 @@
-myApp.controller('RosterController', ['$scope', 'mainService', function($scope, mainService) {
+myApp.controller('RosterController', ['$scope', 'mainService', '$http', function($scope, mainService, $http) {
     $scope.selectedTeam = mainService.getSelectedTeam();
+
+    $scope.nflTeamsRoster = null;
+    $scope.selectedTeamRoster = null;
+
+    $http.get('data/rosters/nfl_rosters.json').then(function(response) {
+        $scope.nflTeamsRoster = response.data;
+        $scope.selectedTeamRoster = $scope.nflTeamsRoster[$scope.selectedTeam.name].roster;
+    });
 }]);
