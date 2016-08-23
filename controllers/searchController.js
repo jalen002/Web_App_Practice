a96431acf51a2764efe5e18881192c5b366b5ef9
jalen002 <jhalvey08@gmail.com>
2016-08-23 16:35:14 -0700

    implemented name search for players

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -9,6 +9,31 @@ myApp.controller('SearchController', ['$scope', 'mainService', function($scope, 
     $scope.searchCollege = null;
     $scope.searchExperience = null;
 
+    $scope.searchResults = [];
+
+    $scope.searchClicked = function(){
+        $scope.nflRoster = mainService.getNFLRoster();
+        $scope.searchResults = [];
+        if($scope.searchName != null){
+            for(var k in $scope.nflRoster){
+                for(x = 0; x < $scope.nflRoster[k].roster.length; x++){
+                    if($scope.nflRoster[k].roster[x].NAME.toUpperCase().includes($scope.searchName.toUpperCase())){
+                        $scope.searchResults.push({ "TEAM" : $scope.nflRoster[k].name,
+                                                    "NO" : $scope.nflRoster[k].roster[x].NO,
+                                                    "NAME" : $scope.nflRoster[k].roster[x].NAME,
+                                                    "POS" : $scope.nflRoster[k].roster[x].POS,
+                                                    "AGE" : $scope.nflRoster[k].roster[x].AGE,
+                                                    "HT" : $scope.nflRoster[k].roster[x].HT,
+                                                    "WT" : $scope.nflRoster[k].roster[x].WT,
+                                                    "EXP" : $scope.nflRoster[k].roster[x].EXP,
+                                                    "COLLEGE" : $scope.nflRoster[k].roster[x].COLLEGE});
+                    }
+                }
+            }
+        }
+        return $scope.searchResults;
+    }
+
 }]).config(function($mdThemingProvider) {
         // Configure a dark theme with primary foreground yellow
         $mdThemingProvider.theme('docs-dark', 'default')
